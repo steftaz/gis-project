38cd65628e640ed694c228188603399755dabecb
steftaz <73255450+steftaz@users.noreply.github.com>
2020-10-22 10:17:36 +0200

    Update settings.py
    fixed security issues

--- gisproject/settings.py
+++ gisproject/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o28_+gdwi98zv@&i6urdpm2fcd!2582@az^l%wdxewt=gb1!w2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 STATIC_URL = '/static/'
